66a216c340a50195a54c8d3e6108a1dd53c21f48
tvaidotas <tadas.vaidotas@gmail.com>
2019-09-10 13:31:59 +0100

    added a test for the App class created HomeControllerTest class

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -4,16 +4,15 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    App app;
+
     @Test
     public void shouldAnswerWithTrue()
     {
+        app = new App();
+        app.main(new String[]{});
     }
 }
